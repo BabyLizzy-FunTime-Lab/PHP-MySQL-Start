7d02b8ce7884075f70931595e0611b5c0af400e1
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2021-02-24 14:55:16 +0100

    setting up workspace, added some style, index.php works

--- index.php
+++ index.php
@@ -3,11 +3,25 @@
 
 <head>
     <title>PHP & SQL Start AMPPS</title>
+    <meta charset="utf-8">
+    <meta name="keywords" content="PHP, SQL, Start">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Orbitron&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Tomorrow&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="Style/normalize.css">
+	<link rel="stylesheet" type="text/css" href="Style/main.css">
+    
 </head>
 
 <body>
-    <h1>PHP & SQL Start The Real AMPPS Server</h1>
+	<header id="pageheader">
+		<h1>AMPPS Server</h1>
+    	<p>Let's do some PHP & SQL</p>
+	</header>
+    
     <div id="HelloWorld-PHP">
         <?php include 'PHP/helloWorld.php';?>
     </div>
