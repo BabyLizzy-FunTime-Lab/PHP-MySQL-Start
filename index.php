1322ccd777995d11ba76205d47529b20103445a9
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2021-02-24 14:30:10 +0100

    renamed index.html index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP & SQL Start AMPPS</title>
+
+</head>
+
+<body>
+    <h1>PHP & SQL Start The Real AMPPS Server</h1>
+    <div id="HelloWorld-PHP">
+        <?php include 'PHP/helloWorld.php';?>
+    </div>
+
+
+</body>
+
+</html>
