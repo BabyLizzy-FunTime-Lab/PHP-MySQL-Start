9d107b49e2c1d590a44dd5d93f7357cf79de057f
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2021-03-18 23:24:25 +0100

    set script link to the front JS

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 	<link rel="stylesheet" type="text/css" href="Style/normalize.css">
 	<link rel="stylesheet" type="text/css" href="Style/main.css">
-
+	<script src="JS/front.js" async></script>
 	<!-- The echo time forces the css to reload everytime -->
     
 </head>
