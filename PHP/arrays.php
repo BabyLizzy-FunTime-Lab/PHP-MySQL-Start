0a99a45a2482952804bc4f23a9ba79bcfa0035a9
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2021-05-14 15:23:52 +0200

    done with php arrays

--- PHP/arrays.php
+++ PHP/arrays.php
@@ -1,5 +1,5 @@
 <?php
-	echo "<h3>Regular Array.</h3>";
+	echo "<h3>Numeric (Regular) Array.</h3>";
 	$paper[] = "copier";
 	$paper[] = "inkjet";
 	$paper[] = "laser";
@@ -33,3 +33,25 @@
 		echo "$brand: $model_list<br>";
 	}
 
+	echo "<h3>Mutidimencional Arrays</h3>";
+	echo "<p>This is a 2D tabel with the tabel of 1 to 10 in it.</p>";
+	echo "<p>It's 10 numeric arrays within an associative array</p>";
+	function tafelVanTien_Array($range) {
+		$tafel_array = array();
+		for ($i=1; $i < $range + 1; $i++) { 
+			for ($j=1; $j < $range + 1; $j++) {
+				// Here I make uniqe array names with the counter.
+				// It will retain it's old value on the new loop.
+				${"tafel_$i"}[0] = 0;
+				${"tafel_$i"}[] = $i * $j;
+				if (count(${"tafel_$i"}) == 11) {
+					// Here I make an associative array.
+					$tafel_array["tafel_$i"] = ${"tafel_$i"};
+				}
+			}
+		}
+		return $tafel_array;
+	}
+	print_r(tafelVanTien_Array(10));
+
+?>
