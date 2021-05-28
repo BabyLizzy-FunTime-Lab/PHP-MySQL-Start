d7d5d0a236d335bcfcad9b47f18d0a8b1258c103
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2021-05-28 17:03:59 +0200

    making examples of the practical PHP chapter

--- PHP/practical_PHP.php
+++ PHP/practical_PHP.php
@@ -4,6 +4,17 @@
 	$pi = pi();
 	printf("The simpel number pi = %.2f<br>", $pi);
 	printf("But pi is actually much more complex = %.12f<br>", $pi);
-	echo "<h3>Date and Time</h3>";
-	echo "The current time in seconds. Starting on 01/01/1970 = " . time() . "<br>";
+	echo "<h3>Date and Time</h3>" . 
+		"The current time in seconds. Starting on 01/01/1970 = " . time() . "<br>"
+		. "The current time and date = " . date("l F j, Y - G:i:s", time()) . "<br>"
+		. "With checkdate() you can check if a date can exsist.<br>";
+	$month = 9;
+	$day = 19;
+	$year = 1983;
+	if (checkdate($month, $day, $year)) {
+		echo "Date in the code is valid.";
+	} else {
+		echo "Date in the code is NOT valid";
+	}
+
 ?>
