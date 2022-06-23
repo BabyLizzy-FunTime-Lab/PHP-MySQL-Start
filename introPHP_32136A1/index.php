85f5d7947dd7e25c6973edaa0d1854af03b4ff92
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2022-06-23 16:54:56 +0200

    making the first inzend

---
+++ introPHP_32136A1/index.php
@@ -0,0 +1,14 @@
+<
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Inzendopdracht: 32136A1</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello World";
+        ?>
+    </body>
+</html>
