7f747c941c841bd31e3d3970be253ef18e0c7de6
Darthporkius <cholochullo@hotmail.com>
2022-07-01 11:47:56 +0200

    php opdracht 1 klaar

--- introPHP_32136A1/index.php
+++ introPHP_32136A1/index.php
@@ -1,4 +1,7 @@
 <?php
+    $mugshot = "image/mugshot.jpg";
+    $videohallo = "video/helloworld.mp4";
+
     echo <<<_END
     <!DOCTYPE html>
     <html lang="nl">
@@ -10,8 +13,13 @@
             <title>Inzendopdracht: 32136A1</title>
         </head>
         <body>
-            <header id="hoofd-header">
-                <h1>Hello World</h1>
+            <header id="banner-container">
+                <div id="banner">
+                    <h1>Hello World</h1>
+                    <picture id="mugshot-container">
+                        <img id="mugshot" src=$mugshot alt="Foto van José">
+                    </picture>
+                </div>
             </header>
             <article id="about-container">
                 <div id="about">
@@ -23,11 +31,8 @@
                         Ik heb certificaten in HTML5, CSS3 en javascript. Binnen de komende maanden zal ik certificaten in PHP & SQL behalen. Als afgestudeerde frontend developer bij de LOI zal ik ook bekwaam zijn in vue.js.
                     </p>
                 </div>
-                <picture id="mugshot-container">
-                    <img id="mugshot" src="image/mugshot.jpg" alt="Foto van José">
-                </picture>
                 <video id="hallovideo" controls>
-                    <source src="video/helloworld.mp4" type="video/mp4">
+                    <source src=$videohallo type="video/mp4">
                 </video>
             </article>
         </body>
