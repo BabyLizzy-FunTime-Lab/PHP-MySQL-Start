5e10f688761b014f48b587a851081933a77e2d50
Darthporkius <cholochullo@hotmail.com>
2022-07-01 16:26:51 +0200

    practice loops

---
+++ oefenen/index.php
@@ -0,0 +1,30 @@
+<?php
+	$root = 8;
+	$multiplyer = 1;
+
+	if ($root < 13) {
+			
+	}
+
+	for ($i=1; $i < 10; $i++) { 
+		
+	}
+
+	echo <<<_END
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<title>oefenen</title>
+		</head>
+		<body>
+			<h1>Oefenen met PHP</h1>
+			<article>
+				<h2>Loops</h2>
+				<p>De tafels rond de tafel van 10:</p>
+			</article>
+		</body>
+		</html>
+_END; 
+?>
