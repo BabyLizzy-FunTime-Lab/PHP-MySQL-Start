da6be2a9fdc8324be6e5ffb86a544063197bfa97
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2022-07-04 15:45:02 +0200

    klaar met loops oefenen

--- oefenen/index.php
+++ oefenen/index.php
@@ -1,13 +1,14 @@
 <?php
-	$root = 8;
-	$multiplyer = 1;
-
-	if ($root < 13) {
-			
-	}
-
-	for ($i=1; $i < 10; $i++) { 
-		
+	// De tafels rond de tafel van 10
+	// De antwoorden worden in een tabel gezet
+	for ($M_cand=1; $M_cand < 11; $M_cand++) { 
+		static $table_data;
+		$table_data = $table_data . "<tr>";
+		for ($M_plier=8; $M_plier < 13; $M_plier++) { 
+			$product = $M_cand * $M_plier;
+			$table_data = $table_data . "<td>" . $M_cand . " * " .  $M_plier . " = " . $product . "</td>";
+		}
+		$table_data = $table_data . "</tr>";
 	}
 
 	echo <<<_END
@@ -22,7 +23,16 @@
 			<h1>Oefenen met PHP</h1>
 			<article>
 				<h2>Loops</h2>
-				<p>De tafels rond de tafel van 10:</p>
+				<table>
+					<thead> 
+						<th colspan="5">
+							De tafels rond de tafel van 10:
+						</th>
+					</thead>
+					<tbody>
+						$table_data
+					</tbody>
+				</table>
 			</article>
 		</body>
 		</html>
